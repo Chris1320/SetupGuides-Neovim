44047aeac4a54af18dd361efd68af3a5c4d73b62
Chris1320 <chris1320is@protonmail.com>
2022-08-12 16:21:55 +0800

    [WIP] Rewriting the init file.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- neovim-init/init.lua
+++ neovim-init/init.lua
@@ -9,54 +9,7 @@
 -- vim.opt  -- options (`set` command in vim)
 
 local vars = require("vars")  -- load variables
-
-vim.g.mapleader = vars["leaderkey"]                         -- Set leader key.
-vim.cmd("filetype plugin on")                               -- enable loading plugins for specific file types.
-
--- Editor-related configuration
-vim.opt.number = true                                       -- enable line numbers.
-vim.opt.showmatch = true                                    -- show matching brackets.
-vim.opt.cursorline = true                                   -- enable cursor line.
-vim.opt.wrap = false                                        -- Disable line wrapping.
-vim.opt.wildmode = {"longest", "list"}                      -- get bash-like tab completions.
-vim.opt.mouse = 'a'                                         -- enable usage of mouse in all modes.
-vim.opt.clipboard = "unnamedplus"                           -- enable usage of system clipboard.
-vim.opt.ttyfast = true                                      -- indicate fast terminal connection to speed up scrolling.
-vim.opt.termguicolors = true                                -- enable for consistent color scheme across terminals.
-vim.cmd("syntax on")                                        -- enable syntax highlighting
-vim.cmd("highlight LineNr ctermfg=" .. vars["ctermfg"])     -- Set the color of the line numbers
-
--- Search-related configuration
-vim.opt.hlsearch = true                                     -- highlight search results.
-vim.opt.incsearch = true                                    -- enable incremental search.
-vim.opt.smartcase = true                                    -- smart case searching.
-vim.opt.ignorecase = true                                   -- case insensitive when searching.
-vim.opt.magic = true                                        -- enable regex.
-
--- Indentation-related configuration
-vim.cmd("filetype plugin indent on")                        -- allow auto-indentation depending on file type.
-vim.opt.autoindent = true                                   -- Enable auto indent.
--- vim.opt.smartindent = true                               -- Enable smart indent.
-vim.opt.indentexpr = "nvim_treesitter#indentexpr()"         -- Use treesitter for indentation.
-vim.opt.expandtab = vars["use_spaces"]                      -- converts tabs to white space.
-vim.opt.shiftwidth = 4                                      -- width for autoindents.
-vim.opt.tabstop = 4                                         -- number of columns occupied by a tab.
-vim.opt.softtabstop = 4                                     -- see multiple spaces as tabstops so <BS> does the right thing.
-
-vim.opt.foldmethod = "expr"                                 -- use treesitter to determine where to fold.
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"             -- the expression to use for folding.
-
-vim.opt.foldlevel = 99                                      -- fold level.
-vim.opt.list = true                                         -- enable display of unprintable characters.
-if vars["show_eols"] then                                   -- Enable if you want to display spaces, trailing spaces, and EOLs.
-    vim.opt.listchars:append("eol:" .. vars["eol_char"])
-end
-if vars["show_spaces"] then
-    vim.opt.listchars:append("space:" .. vars["space_char"])
-end
-if vars["show_trails"] then
-    vim.opt.listchars:append("trail:" .. vars["trail_char"])
-end
+require("options")  -- Set options
 
 -- Start packer
 require("plugins")
