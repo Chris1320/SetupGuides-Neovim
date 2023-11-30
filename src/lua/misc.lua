5eacb27757a90c5f3f8eafd9fa317d0101409075
Chris1320 <chris1320is@protonmail.com>
2023-11-30 17:59:15 +0800

    fix(misc): `vim.fs.dirname` expected string got table
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- src/lua/misc.lua
+++ src/lua/misc.lua
@@ -15,14 +15,14 @@ return {
 
     --- Detects the project root of the current buffer.
     --- @param landmarks table A table containing a list of files that are commonly found in the root directory.
-    --- @param dir_context string The directory context to start the search from. (Default: current buffer's directory)
+    --- @param dir_context? string The directory context to start the search from. (Default: current buffer's directory)
     ---
     --- @return string|nil found_dir The root project of the current buffer. nil if nothing is found.
     detectProjectRoot = function(landmarks, dir_context)
         local dir_context = dir_context or vim.fn.expand("%:p:h") --- @diagnostic disable-line: redefined-local
         local found_landmark = vim.fs.find(landmarks, { upward = true, path = dir_context })
         if found_landmark then
-            return vim.fs.dirname(found_landmark)
+            return vim.fs.dirname(found_landmark[1])
         end
     end,
 
