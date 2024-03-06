4aae7178e6dd4539d29116b85977f2b7b871c4e1
chris1320 <chris1320is@protonmail.com>
2024-03-06 16:21:28 +0800

    feat: update `options.lua`
    I initally planned to remove `vars.lua` completely, but I think that
    we'll need it to share the same settings across different plugins. It
    might be removed in the future, though.
    
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- src/lua/config/options.lua
+++ src/lua/config/options.lua
@@ -1,3 +1,46 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local g = vim.g
+local o = vim.opt
+
+o.scrolloff = 5
+o.timeout = true
+o.timeoutlen = 300
+o.updatetime = 50
+
+o.swapfile = false -- disable swap files.
+-- o.backup = false  -- disable backup files.
+-- o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+o.undofile = true
+
+-- Check if running on WSL.
+-- Source: https://mitchellt.com/2022/05/15/WSL-Neovim-Lua-and-the-Windows-Clipboard.html
+if os.getenv("WSL_DISTRO_NAME") ~= nil then
+    local wsl_copy_cmd = { "clip.exe" }
+    local wsl_paste_cmd = {
+        "powershell.exe",
+        "-c",
+        '[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    }
+    g.clipboard = {
+        name = "WSL Clipboard",
+        copy = { ["+"] = wsl_copy_cmd, ["*"] = wsl_copy_cmd },
+        paste = { ["+"] = wsl_paste_cmd, ["*"] = wsl_paste_cmd },
+        cache_enabled = true,
+    }
+end
+
+g.clipboard = "unnamed"
+
+o.hlsearch = true -- highlight search results.
+o.incsearch = true -- enable incremental search.
+o.magic = true -- enable regex.
+
+o.shiftwidth = require("config.vars").appearance.tab_width
+o.tabstop = require("config.vars").appearance.tab_width
+o.softtabstop = require("config.vars").appearance.tab_width
+
+o.showmatch = true -- show matching brackets.
+o.colorcolumn = require("config.vars").appearance.colorcolumn.default_columns
---
+++ src/lua/config/vars.lua
@@ -0,0 +1,8 @@
+return {
+    appearance = {
+        tab_width = 4, -- 4 spaces is just the right amount of spaces... fight me.
+        colorcolumn = {
+            default_columns = { 80 },
+        },
+    },
+}
