586129ca22cfb66c8a0bf07e68a4d95f2506db03
chris1320 <chris1320is@protonmail.com>
2024-03-08 14:19:50 +0800

    feat(telescope): update telescope config
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ src/lua/plugins/telescope.lua
@@ -0,0 +1,54 @@
+return {
+    "nvim-telescope/telescope.nvim",
+
+    enabled = true,
+    cmd = "Telescope",
+    dependencies = {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "nvim-treesitter",
+        "trouble.nvim",
+    },
+    opts = function()
+        local trouble = require("trouble")
+        local telescope_actions = require("telescope.actions")
+        return {
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<esc>"] = telescope_actions.close,
+                        ["<C-t>"] = trouble.open_with_trouble,
+                    },
+                    n = { ["<C-t>"] = trouble.open_with_trouble },
+                },
+            },
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
+                },
+            },
+        }
+    end,
+    keys = {
+        {
+            "<leader>t",
+            function()
+                require("telescope.builtin").builtin(require("telescope.themes").get_dropdown({ previewer = false }))
+            end,
+            "n",
+            desc = "Telescope",
+        },
+        {
+            "<leader>gb",
+            function()
+                require("telescope.builtin").git_branches(require("telescope.themes").get_dropdown())
+            end,
+            "n",
+            desc = "branches",
+        },
+    },
+}
