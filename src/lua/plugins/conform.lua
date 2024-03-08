806075c5076138392a33455fcd431ea15b7bd406
chris1320 <chris1320is@protonmail.com>
2024-03-08 14:44:40 +0800

    feat(conform.nvim): updated conform.nvim config
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ src/lua/plugins/conform.lua
@@ -0,0 +1,83 @@
+return {
+    "stevearc/conform.nvim",
+
+    enabled = true,
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    ---@class ConformOpts
+    opts = {
+        -- LazyVim will use these options when formatting with the conform.nvim formatter
+        format = {
+            timeout_ms = 3000,
+            async = false, -- not recommended to change
+            quiet = false, -- not recommended to change
+        },
+        ---@type table<string, conform.FormatterUnit[]>
+        formatters_by_ft = {
+            arduino = { "clang_format" },
+            bash = { "shfmt" },
+            c = { "clang_format" },
+            cpp = { "clang_format" },
+            cs = { "csharpier" },
+            css = { "prettierd" },
+            html = { "prettierd" },
+            javascript = { "prettierd" },
+            json = { "prettierd" },
+            lua = { "stylua" },
+            luau = { "stylua" },
+            markdown = { "prettierd" },
+            python = { "black", "isort" },
+            scss = { "prettierd" },
+            sh = { "shfmt" },
+            typescript = { "prettierd" },
+            vue = { "prettierd" },
+            yaml = { "prettierd" },
+            zsh = { "shfmt" },
+        },
+        -- The options you set here will be merged with the builtin formatters.
+        -- You can also define any custom formatters here.
+        ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+        formatters = {
+            black = {
+                -- selene: allow(unused_variable)
+                cwd = function(...) --- @diagnostic disable-line: unused-vararg
+                    return require("config.misc").detectProjectRoot({
+                        "pyproject.toml",
+                        "setup.cfg",
+                        "setup.py",
+                        ".git",
+                    }) or vim.fn.getcwd()
+                end,
+            },
+            clang_format = { prepend_args = { "--fallback-style=Google" } },
+            isort = { prepend_args = { "--profile", "black" } },
+            shfmt = { prepend_args = { "--indent", "4" } },
+            stylua = {
+                prepend_args = function()
+                    local result = { "--color", "Never" }
+
+                    -- Do not override project config if they have one.
+                    if
+                        vim.fn.filereadable(vim.fn.getcwd() .. "/.stylua.toml") ~= 1
+                        and vim.fn.filereadable(vim.fn.getcwd() .. "/stylua.toml") ~= 1
+                    then
+                        table.insert(result, "--call-parentheses")
+                        table.insert(result, "Always")
+                        table.insert(result, "--indent-type")
+                        table.insert(result, "Spaces")
+                    end
+
+                    return result
+                end,
+                -- selene: allow(unused_variable)
+                cwd = function(...) --- @diagnostic disable-line: unused-vararg
+                    return require("config.misc").detectProjectRoot({
+                        ".stylua.toml",
+                        "stylua.toml",
+                        ".git",
+                    }) or vim.fn.getcwd()
+                end,
+            },
+        },
+    },
+}
