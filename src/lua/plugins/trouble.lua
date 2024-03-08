3fda3a018bf86df228b3e35ca897d7e491841703
chris1320 <chris1320is@protonmail.com>
2024-03-08 14:01:27 +0800

    feat(trouble): update trouble config
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ src/lua/plugins/trouble.lua
@@ -0,0 +1,16 @@
+return {
+    "folke/trouble.nvim",
+
+    enabled = true,
+    cmd = { "TroubleToggle", "Trouble" },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        use_diagnostic_signs = true,
+        position = "bottom",
+        icons = true,
+        group = true,
+        padding = true,
+        indent_lines = true,
+        auto_preview = true,
+    },
+}
