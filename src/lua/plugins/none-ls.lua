49ac495e56ff2cd5443f6c17667432e871249752
chris1320 <chris1320is@protonmail.com>
2024-03-10 11:00:33 +0800

    feat(none-ls): add formatters to none-ls config
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- src/lua/plugins/none-ls.lua
+++ src/lua/plugins/none-ls.lua
@@ -5,5 +5,15 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    opts = { sources = { require("null-ls").builtins.formatting.black } },
+    opts = {
+        sources = {
+            require("null-ls").builtins.formatting.black,
+            require("null-ls").builtins.formatting.clang_format,
+            require("null-ls").builtins.formatting.csharpier,
+            require("null-ls").builtins.formatting.isort,
+            require("null-ls").builtins.formatting.prettierd,
+            require("null-ls").builtins.formatting.shfmt,
+            require("null-ls").builtins.formatting.stylua,
+        },
+    },
 }
