c9ae746217ff5842d99f464d644ce958d85f7941
chris1320 <chris1320is@protonmail.com>
2024-03-12 18:52:55 +0800

    fix(none-ls): fix first-time setup error
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- src/lua/plugins/none-ls.lua
+++ src/lua/plugins/none-ls.lua
@@ -5,17 +5,25 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    opts = {
-        sources = {
-            require("null-ls").builtins.formatting.black,
-            require("null-ls").builtins.formatting.clang_format,
-            require("null-ls").builtins.formatting.csharpier,
-            require("null-ls").builtins.formatting.isort,
-            require("null-ls").builtins.formatting.prettierd,
-            require("null-ls").builtins.formatting.shfmt,
-            require("null-ls").builtins.formatting.stylua,
+    opts = function()
+        local ok, nls = pcall(require, "null-ls")
+        if not ok then
+            vim.notify("Please restart Neovim to use none-ls.")
+            return {}
+        end
 
-            require("null-ls").builtins.diagnostics.markdownlint,
-        },
-    },
+        return {
+            sources = {
+                nls.builtins.formatting.black,
+                nls.builtins.formatting.clang_format,
+                nls.builtins.formatting.csharpier,
+                nls.builtins.formatting.isort,
+                nls.builtins.formatting.prettierd,
+                nls.builtins.formatting.shfmt,
+                nls.builtins.formatting.stylua,
+
+                nls.builtins.diagnostics.markdownlint,
+            },
+        }
+    end,
 }
