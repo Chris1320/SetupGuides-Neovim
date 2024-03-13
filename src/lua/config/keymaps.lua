f974777a9685036396a66c480e2aac047feeb3a1
chris1320 <chris1320is@protonmail.com>
2024-03-13 14:36:59 +0800

    style(keymaps): suppress selene `multiple_statements` warning
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- src/lua/config/keymaps.lua
+++ src/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
+--# selene: allow(multiple_statements)
+
 -- Keyvim.keymap.sets are automatically loaded on the VeryLazy event
 -- Default keyvim.keymap.sets that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keyvim.keymap.sets here
