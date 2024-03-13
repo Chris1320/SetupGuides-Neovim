42c36de910c1917183b70a295a9fcc72810657bd
chris1320 <chris1320is@protonmail.com>
2024-03-13 13:32:47 +0800

    feat: add presence.nvim plugin
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ src/lua/plugins/presence.lua
@@ -0,0 +1,10 @@
+return {
+    "andweeb/presence.nvim",
+
+    enabled = true,
+    event = "VeryLazy",
+    opts = {
+        neovim_image_text = "I use Neovim btw",
+        -- log_level = "error",
+    },
+}
