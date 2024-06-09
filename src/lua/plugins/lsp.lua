6b9a070bc8a9701fef486d44c1c85f78310d2f80
chris1320 <chris1320is@protonmail.com>
2024-06-09 23:33:30 +0800

    feat(lsp): updated LSP configs
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ src/lua/plugins/lsp.lua
@@ -0,0 +1,73 @@
+return {
+    "neovim/nvim-lspconfig",
+
+    opts = {
+        -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+        -- Be aware that you also will need to properly configure your LSP server to
+        -- provide the inlay hints.
+        inlay_hints = { enabled = true },
+        -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
+        -- Be aware that you also will need to properly configure your LSP server to
+        -- provide the code lenses.
+        codelens = { enabled = true },
+        servers = {
+            bashls = {
+                filetypes = { "sh", "bash" },
+                single_file_support = true,
+            },
+            lua_ls = { settings = { Lua = { telemetry = { enable = false } } } },
+            omnisharp = {
+                cmd = {
+                    vim.fn.has("win32") == 1 and "OmniSharp.exe" or "omnisharp",
+                    "--languageserver",
+                    "--hostPID",
+                    tostring(vim.fn.getpid()),
+                },
+                enable_editorconfig_support = true,
+            },
+            pyright = {
+                single_file_support = true,
+                settings = {
+                    python = {
+                        analysis = {
+                            autoImportCompletions = true,
+                            autoSearchPaths = true,
+                            diagnosticMode = "workspace",
+                            diagnosticSeverityOverrides = {
+                                reportDeprecated = "warning",
+                                reportImplicitOverride = "information",
+                                reportShadowedImports = "information",
+                                reportUninitializedInstanceVariable = "information",
+                                reportUnnecessaryTypeIgnoreComment = "information",
+                            },
+                            -- -- Add the virtual environment.
+                            -- extraPaths = (
+                            --     vim.fn.getenv("VIRTUAL_ENV") ~= vim.NIL
+                            --     and { vim.fn.getenv("VIRTUAL_ENV") .. "/lib/python3.11/site-packages" }
+                            -- ) or {},
+                            typeCheckingMode = "strict",
+                            useLibraryCodeForTypes = true,
+                        },
+                    },
+                },
+            },
+            ruff_lsp = {
+                -- keys = {
+                --     {
+                --         "<leader>co",
+                --         function()
+                --             vim.lsp.buf.code_action({
+                --                 apply = true,
+                --                 context = {
+                --                     only = { "source.organizeImports" },
+                --                     diagnostics = {},
+                --                 },
+                --             })
+                --         end,
+                --         desc = "Organize Imports",
+                --     },
+                -- },
+            },
+        },
+    },
+}
