89f2ad8d752350ee9025cc6fc7ebe65fcb53a9af
chris1320 <chris1320is@protonmail.com>
2024-06-11 16:20:49 +0800

    fix(lsp): disable codelens for now
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- src/lua/plugins/lsp.lua
+++ src/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
         -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
         -- Be aware that you also will need to properly configure your LSP server to
         -- provide the code lenses.
-        codelens = { enabled = true },
+        codelens = { enabled = false },
         servers = {
             bashls = {
                 filetypes = { "sh", "bash" },
