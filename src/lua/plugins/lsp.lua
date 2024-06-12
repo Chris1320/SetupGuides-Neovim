7c3aa17c96cbac53042e37f4878f0acb6e453e66
chris1320 <chris1320is@protonmail.com>
2024-06-12 09:30:22 +0800

    fix(lsp): leave defaults for inlay hints and codelens
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- src/lua/plugins/lsp.lua
+++ src/lua/plugins/lsp.lua
@@ -5,11 +5,11 @@ return {
         -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
         -- Be aware that you also will need to properly configure your LSP server to
         -- provide the inlay hints.
-        inlay_hints = { enabled = false },
+        -- inlay_hints = { enabled = false },
         -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
         -- Be aware that you also will need to properly configure your LSP server to
         -- provide the code lenses.
-        codelens = { enabled = false },
+        -- codelens = { enabled = false },
         servers = {
             bashls = {
                 filetypes = { "sh", "bash" },
