911e714f4ca0e7696f7a8e1a78589e95b166c0b6
chris1320 <chris1320is@protonmail.com>
2024-06-12 09:29:26 +0800

    fix(lsp): disable inlay hints for now
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- src/lua/plugins/lsp.lua
+++ src/lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
         -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
         -- Be aware that you also will need to properly configure your LSP server to
         -- provide the inlay hints.
-        inlay_hints = { enabled = true },
+        inlay_hints = { enabled = false },
         -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
         -- Be aware that you also will need to properly configure your LSP server to
         -- provide the code lenses.
