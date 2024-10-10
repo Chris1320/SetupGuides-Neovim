938905ea3c8c4786c802487398ee286d9fd43cfb
chris1320 <chris1320is@protonmail.com>
2024-10-10 21:44:53 +0800

    fix(conform,vim-arduino): update deprecated method calls
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- src/lua/plugins/conform.lua
+++ src/lua/plugins/conform.lua
@@ -17,7 +17,7 @@ return {
     ---@class ConformOpts
     opts = {
         -- LazyVim will use these options when formatting with the conform.nvim formatter
-        format = {
+        default_format_opts = {
             timeout_ms = 3000,
             async = false, -- not recommended to change
             quiet = false, -- not recommended to change
