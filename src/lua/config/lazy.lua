92f3da20ea17bdc6a60fa50f00f582fbf478a947
Chris1320 <chris1320is@protonmail.com>
2024-12-13 12:12:08 +0800

    refactor: remove duplicate lazy extras
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- src/lua/config/lazy.lua
+++ src/lua/config/lazy.lua
@@ -31,7 +31,6 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.util.dot" },
         { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 
-        { import = "lazyvim.plugins.extras.lang.tailwind" },
         { import = "lazyvim.plugins.extras.lang.clangd" },
         { import = "lazyvim.plugins.extras.lang.git" },
         { import = "lazyvim.plugins.extras.lang.java" },
