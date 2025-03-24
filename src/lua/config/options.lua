c28491321ca5755160631597ca7a609b6c5676d0
Chris1320 <me@chrisfromnowhere.me>
2025-03-25 01:42:46 +0800

    feat: re-add options
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- src/lua/config/options.lua
+++ src/lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- local g = vim.g
+local o = vim.opt
+
+o.hlsearch = true -- highlight search results.
+o.incsearch = true -- enable incremental search.
+o.magic = true -- enable regex.
+
+o.shiftwidth = 4 -- number of spaces to use for each step of (auto)indent.
+o.tabstop = 4 -- number of spaces that a <Tab> in the file counts for.
+o.softtabstop = 4 -- number of spaces that a <Tab> counts for while editing.
+
+o.showmatch = true -- show matching brackets.
+o.colorcolumn = { 80 } -- Which columns should be highlighted
