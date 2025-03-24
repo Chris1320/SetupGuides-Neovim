b8f451cefd0164c6b9f773ea69562d22a345b863
Chris1320 <me@chrisfromnowhere.me>
2025-03-25 02:18:10 +0800

    feat: re-add mason.nvim configuration
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

---
+++ src/lua/plugins/mason.nvim.lua
@@ -0,0 +1,15 @@
+return {
+    "williamboman/mason.nvim",
+
+    opts = {
+        ui = {
+            check_outdated_packages_on_open = true,
+            icons = {
+                -- you can change these icons to whatever you want.
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗",
+            },
+        },
+    },
+}
